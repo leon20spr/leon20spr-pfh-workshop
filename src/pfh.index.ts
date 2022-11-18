c99f18443c58f68d06148746f620923284a3c125
Leon Spreckels <110732125+leon20spr@users.noreply.github.com>
2022-11-18 14:04:17 +0100

    added pfh.index.ts / index

---
+++ src/pfh.index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import twig from "twig";
+
+class PFHIndex {
+    public constructor() {}
+
+    /**
+     * To start the application
+     */
+    public async startApplication(): Promise<void> {
+        try {
+            
+        } catch (error: any) {
+            throw error;
+        }
+    }
+}
+
+const pfh = new PFHIndex();
+
+export default pfh;
+
+pfh.startApplication();
