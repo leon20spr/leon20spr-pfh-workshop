6115ccb5be40ce010025795c8daf5b21bb779e5e
Leon Spreckels <110732125+leon20spr@users.noreply.github.com>
2022-11-18 14:18:42 +0100

    edited helpers.index.ts / helpers
    [+] console
    [+] typeorm

--- src/pfh-helpers/helpers.index.ts
+++ src/pfh-helpers/helpers.index.ts
@@ -1,10 +1,19 @@
-import PFHHelpersHandlerDateTimeIndex from "./helpers-handlers/handler.datetime";
+import PFHHelpersHandlerDateTimeIndex from "./helpers-handlers/handler.datetime.js";
+import PFHHelpersHandlerConsoleIndex from "./helpers-handlers/handler.console.js";
+
+import PFHHelpersHandlerTypeORMIndex from "./helpers-handlers/handler.typeorm.js";
 
 class PFHHelpersIndex {
     public datetime: PFHHelpersHandlerDateTimeIndex;
+    public console: PFHHelpersHandlerConsoleIndex;
+
+    public typeorm: PFHHelpersHandlerTypeORMIndex;
 
     public constructor() {
         this.datetime = new PFHHelpersHandlerDateTimeIndex();
+        this.console = new PFHHelpersHandlerConsoleIndex();
+
+        this.typeorm = new PFHHelpersHandlerTypeORMIndex();
     }
 }
 
