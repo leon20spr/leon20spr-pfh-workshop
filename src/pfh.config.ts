95035421cf784c917b4e466d45b530c561d0526c
Leon Spreckels <110732125+leon20spr@users.noreply.github.com>
2022-11-18 14:18:18 +0100

    added pfh.config.ts / index

---
+++ src/pfh.config.ts
@@ -0,0 +1,11 @@
+const PFHConfigIndex = {
+    typeorm: {
+        host: "localhost",
+        port: 3306,
+        username: "root",
+        password: "",
+        database: "leon20spr_pfh"
+    }
+}
+
+export default PFHConfigIndex;
