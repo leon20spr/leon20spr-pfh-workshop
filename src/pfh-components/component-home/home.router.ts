68b83c60973af9a2e6cc0efbb459807ce66c4b01
Leon Spreckels <110732125+leon20spr@users.noreply.github.com>
2022-11-18 16:04:42 +0100

    edited home.router.ts / components

--- src/pfh-components/component-home/home.router.ts
+++ src/pfh-components/component-home/home.router.ts
@@ -1,3 +1,13 @@
 import express from "express";
 
 export const PFHComponentHomeRouter = express.Router();
+
+PFHComponentHomeRouter.get("/", (request, response) => {
+    try {
+        response.render("views-home/home.index.twig", {
+            title: "Home"
+        });
+    } catch (error: any) {
+        throw error;
+    }
+});
